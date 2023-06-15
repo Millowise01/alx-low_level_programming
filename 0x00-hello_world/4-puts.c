42fdfd816f7a376f31fae87bda8ae69ce5e63f09
Millowise01 <carewmilfredb@gmail.com>
2023-06-15 08:38:15 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,13 @@
  * 4-puts.c
  */
 
-#include <stdio.h>
 
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0. (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
