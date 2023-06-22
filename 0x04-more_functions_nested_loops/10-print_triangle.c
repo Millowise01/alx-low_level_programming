607a16da0816a570cc048aad796027117328f553
Millowise01 <carewmilfredb@gmail.com>
2023-06-22 10:40:37 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - it Prints a triangle using #
+ * @size: The size of the triangle
+ *
+ * Description: it Prints a triangle with a base and height
+ * of 'size' using the
+ * character '#'. If 'size' is 0 or less.
+ */
+void print_triangle(int size)
+{
+	int i = 0, j, n = size - 1;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
