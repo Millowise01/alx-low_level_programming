4ea69862fc4ac6424665eb805618ff6238d6cb33
Millowise01 <millowise01@yahoo.co>
2023-09-28 11:29:05 -0700

    second bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - to return the value of a bit at an index in a decimal number
+ * @n: it show the number to search
+ * @index: the index of the bit
+ * Return: to value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
