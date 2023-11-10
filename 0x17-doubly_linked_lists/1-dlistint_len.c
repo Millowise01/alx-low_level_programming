3f20d76546b2a44d78b7ce05b70e7b58cc5e4603
Millowise01 <millowise01@yahoo.co>
2023-11-10 00:50:44 -0800

    prints all the elements of a dlistint_t list and returns the number of elements in a linked dlistint_t list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 
 /**
- * print_dlistint - to print all the elements of a
- * dlistint_t list
- * @h: the head of the list
+ * dlistint_len - to count the number of elements
+ * @h: the head of list
  * Return: to the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
     size_t nodes = 0;
 
     while (h)
     {
         nodes++;
-        printf("%d\n", h->n);
         h = h->next;
     }
 
